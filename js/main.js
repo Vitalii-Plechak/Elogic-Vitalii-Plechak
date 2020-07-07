54518e23dc4ff6f355aa608d419b554b4a9f4900
Vitalii-Plechak <vitalii.plechak@gmail.com>
2020-07-07 20:57:50 +0300

    add slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.products-slider__inner').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        dots: true,
+        arrows: false,
+    });
+});
