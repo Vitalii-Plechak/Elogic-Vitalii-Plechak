5a7c01ac230c5ccb0c5fad41c99571fd4a1454a4
Vitalii-Plechak <vitalii.plechak@gmail.com>
2020-07-12 17:29:47 +0300

    add video popup

--- js/main.js
+++ js/main.js
@@ -5,6 +5,36 @@ $(document).ready(function(){
         slidesToScroll: 5,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
-
+// $('.data-fancybox').fancybox({
+//     youtube : {
+//         controls : 0,
+//         showinfo : 0
+//     },
+// });
